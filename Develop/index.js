295aed3ce146acbf28ed57856932b0a5decdd61f
Carson Savage <carsonesavage@gmail.com>
2021-01-18 17:57:24 -0600

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,30 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is the title of the project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the description of the project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is the usage of the project?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What is the Github link of the project?",
+        name: "link"
+    }
 ];
 
 // function to write README file
