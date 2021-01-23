8befa9043262087f8415452b1c6d3ab04a504ff5
Carson Savage <carsonesavage@gmail.com>
2021-01-22 21:14:51 -0600

    working, added output

--- Develop/index.js
+++ Develop/index.js
@@ -1,40 +1,88 @@
-// array of questions for user
-const questions = [
-    {
+const inquirer = require('inquirer');
+const fs = require('fs');
+const api = require('./utils/api');
+const markdown = require('./utils/generateMarkdown');
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+const questions = [{
         type: "input",
-        message: "What is your GitHub username?",
+        message: "Enter your Github username: ",
         name: "username"
     },
     {
         type: "input",
-        message: "What is the title of the project?",
+        message: "Enter your project title: ",
         name: "title"
     },
     {
         type: "input",
-        message: "What is the description of the project?",
+        message: "Enter a project description: ",
         name: "description"
     },
     {
         type: "input",
-        message: "What is the usage of the project?",
-        name: "usage"
+        message: "Enter the installation process: ",
+        name: "installation"
     },
     {
         type: "input",
-        message: "What is the Github link of the project?",
-        name: "link"
+        message: "Enter the usage of your project: ",
+        name: "usage"
+
+    },
+    {
+        type: "input",
+        message: "Enter licenses used for your project: ",
+        name: "licenses"
+    },
+    {
+        type: "input",
+        message: "Enter contributing information: ",
+        name: "contribute"
+
+    }, {
+        type: "input",
+        message: "Enter any tests you are running for your project: ",
+        name: "tests"
     }
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
+async function writeToFile(fileName, data) {
+    writeFileAsync(fileName, data).then(function () {
+        console.log("Successfully wrote README!");
+    }).catch(err => {
+        console.log('err ', err);
+    });
+};
+async function init() {
+    await inquirer
+        .prompt(questions)
+        .then(answers => {
+            const userName = answers.username;
+            const apiData = api.getUser(userName);
+            apiData.then(res => {
+                    const data = {
+                        name: res.name,
+                        username: answers.username,
+                        url: res.html_url,
+                        title: answers.title,
+                        description: answers.description,
+                        installation: answers.installation,
+                        usages: answers.usage,
+                        licenses: answers.licenses,
+                        tests: answers.tests,
+                        contribute: answers.contribute,
+                        email: res.email,
+                        pfp: res.avatar_url
+                    }
+                    const final = markdown(data)
+                    writeToFile("./output/README.md", final);
+                })
+                .catch(err => {
+                    console.log('err ', err);
+                });
+        });
+};
 init();
